21328ca7ff2ce2374e4a98428b1fdbb89bd9268a
Alonso Madrigal <113622881+alonsomadrigal04@users.noreply.github.com>
2025-10-06 17:22:30 +0200

    Pseudo sistema de dialogo

--- Scripts/DialogSystem/TextTyper.cs
+++ Scripts/DialogSystem/TextTyper.cs
@@ -4,12 +4,26 @@ using System.Collections.Generic;
 
 public partial class TextTyper : Control
 {
-    [Export] RichTextLabel textContainer;
-    int defaultTextSpeed;
+    [Export] RichTextLabel textBox;
+    const float textSpeedDefaul = 0.1f;
     Dictionary<string, AudioStream> speakerSounds;
 
-    public void WriteText(string text, int textSpeed, string speaker)
+    public override void _Ready()
     {
+        WriteText("HOLA MUNDO", null);
+    }
 
-    } 
+    public async void WriteText(string text, string speaker, float textSpeed = textSpeedDefaul)
+    {
+        int index = 0;
+        Timer timerSpeech = new() { WaitTime = textSpeed, Autostart = true };
+        textBox.Text = "";
+        foreach (char c in text)
+        {
+            await ToSignal(GetTree().CreateTimer(textSpeed), "timeout");
+            timerSpeech.WaitTime = textSpeed;
+            textBox.Text += text[index];
+            index += 1;
+        }
+    }
 }
