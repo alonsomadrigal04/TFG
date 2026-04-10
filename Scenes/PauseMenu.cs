b85ac4b54ea69ab775361d5c3dea35ef75c906f9
Alonso Madrigal <113622881+alonsomadrigal04@users.noreply.github.com>
2026-04-10 16:57:45 +0200

    Add music/SFX sliders to pause menu
    Introduce music and SFX HSliders to the pause menu and hook them to AudioServer. PauseMenu.cs exports musicSlider and sfxSlider, initializes their values from audio bus volumes, and updates bus volumes (using linear↔dB conversion) on change. Scene changes in Game.tscn add slider and label nodes, a Roboto-Bold font resource, and include the sliders in the PauseLayout node_paths; PauseSounds' bus is set to "SFX". Other minor UI/asset tweaks: TextBox theme font sizes unified to 82, DialogScene shader LOD adjusted and Background layer/BgFrame visibility updated, and cafetera.tres swaps its icon to Assets/Objects/Icons/madalena.jpg.

--- Scenes/PauseMenu.cs
+++ Scenes/PauseMenu.cs
@@ -3,6 +3,12 @@ using System;
 
 public partial class PauseMenu : Control
 {
+    [ExportGroup("Sliders")]
+    [Export] HSlider musicSlider;
+    [Export] HSlider sfxSlider;
+
+    [ExportGroup("Buttons")]
+
     [Export] Button continueButton;
     [Export] Button exitButton;
 
@@ -11,7 +17,58 @@ public partial class PauseMenu : Control
         continueButton.Pressed += ResumeGame;
         exitButton.Pressed += ExitGame;
 
+        sfxSlider.ValueChanged += UpdateSFXVolume;
+        musicSlider.ValueChanged += UpdateMusicVolume;
+
+        InitializeSliders();
     }
+
+    void InitializeSliders()
+    {
+        SetSliderToBusValue(sfxSlider, "SFX");
+        SetSliderToBusValue(musicSlider, "Music");
+    }
+
+    void SetSliderToBusValue(HSlider slider, string busName)
+    {
+        int index = AudioServer.GetBusIndex(busName);
+
+        if (index == -1)
+        {
+            GD.PrintErr($"[Pause Menu] Bus {busName} does not exist");
+            return;
+        }
+
+        float db = AudioServer.GetBusVolumeDb(index);
+        float linear = Mathf.DbToLinear(db);
+
+        slider.SetValueNoSignal(linear);
+    }
+
+    void UpdateSFXVolume(double value)
+    {
+        SetBusVolume("SFX", (float)value);
+    }
+
+    void UpdateMusicVolume(double value)
+    {
+        SetBusVolume("Music", (float)value);
+    }
+
+    void SetBusVolume(string busName, float value)
+    {
+        int index = AudioServer.GetBusIndex(busName);
+
+        if (index == -1)
+        {
+            GD.PrintErr($"[Pause Menu] Bus {busName} does not exist");
+            return;
+        }
+
+        AudioServer.SetBusVolumeDb(index, LinearToDb(value));
+    }
+
+    static float LinearToDb(float linear) =>(linear <= 0) ? -80f : 20f * Mathf.Log(linear);
 
     void ResumeGame() => GameManager.Instance.PauseGame();
 
